c2b6baa7895c61a6691fc37dca4c6856c2c2e833
Francis <frankmk2025@gmail.com>
2026-01-04 18:38:19 +0300

    update

--- apps/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import NotificationViewSet, NotificationTemplateViewSet
-
-router = DefaultRouter()
-router.register(r'notifications', NotificationViewSet, basename='notification')
-router.register(r'templates', NotificationTemplateViewSet, basename='notification-template')
-
-urlpatterns = router.urls
--- apps/students/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, ParentViewSet
-
-router = DefaultRouter()
-router.register(r'students', StudentViewSet, basename='student')
-router.register(r'parents', ParentViewSet, basename='parent')
-
-urlpatterns = router.urls
